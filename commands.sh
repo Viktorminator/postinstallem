1c252d019bce34564a7070769db43b828722c3aa
Viktor Matushevskyi <viktorminator@gmail.com>
2014-09-25 01:21:26 +0300

    Installation of Nodejs

--- commands.sh
+++ commands.sh
@@ -40,3 +40,8 @@ git clone git://github.com/creationix/nvm.git ~/.nvm &&
 printf "\n\n# NVM\nif [ -s ~/.nvm/nvm.sh ]; then\n\tNVM_DIR=~/.nvm\n\tsource ~/.nvm/nvm.sh\nfi" >> ~/.bashrc &&
 NVM_DIR=~/.nvm &&
 source ~/.nvm/nvm.sh &&
+
+echo "Installation of Nodejs" &&
+nvm install v0.10.32 &&
+nvm alias default 0.10 &&
+nvm use 0.10 &&
