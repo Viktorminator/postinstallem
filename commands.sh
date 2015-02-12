4668c53a96400a9c77223339f1f84b482791755e
Viktor Matushevskyi <viktorminator@gmail.com>
2015-02-12 15:42:23 +0200

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -24,7 +24,7 @@ sudo add-apt-repository -y ppa:atareao/sunflower &&
 sudo add-apt-repository -y ppa:webupd8team/brackets &&
 sudo add-apt-repository -y ppa:maarten-baert/simplescreenrecorder &&
 sudo add-apt-repository -y ppa:webupd8team/atom &&
-
+sudo add-apt-repository ppa:skype-wrapper/ppa &&
 
 echo 'deb http://download.videolan.org/pub/debian/stable/ /' | sudo tee -a /etc/apt/sources.list.d/libdvdcss.list &&
 echo 'deb-src http://download.videolan.org/pub/debian/stable/ /' | sudo tee -a /etc/apt/sources.list.d/libdvdcss.list &&
@@ -32,7 +32,7 @@ wget -O - http://download.videolan.org/pub/debian/videolan-apt.asc|sudo apt-key 
 
 sudo apt-get update &&
 
-sudo apt-get install atom chromium-browser wine simplescreenrecorder brackets qt4-qtconfig golang google-chrome-stable mercurial qbittorrent sunflower synaptic vlc gimp gimp-data gimp-plugin-registry gimp-data-extras y-ppa-manager bleachbit openjdk-7-jre oracle-java8-installer icedtea-7-plugin openjdk-7-jre:i386 flashplugin-installer unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller libxine1-ffmpeg mencoder flac faac faad sox ffmpeg2theora libmpeg2-4 uudeview libmpeg3-1 mpeg3-utils mpegdemux liba52-dev mpeg2dec vorbis-tools id3v2 mpg321 mpg123 libflac++6 totem-mozilla icedax lame libmad0 libjpeg-progs libdvdcss2 libdvdread4 libdvdnav4 libswscale-extra-2 ubuntu-restricted-extras ubuntu-wallpapers* git curl libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev build-essential &&
+sudo apt-get install atom chromium-browser wine simplescreenrecorder brackets qt4-qtconfig golang google-chrome-stable mercurial qbittorrent sunflower synaptic vlc gimp gimp-data gimp-plugin-registry gimp-data-extras y-ppa-manager bleachbit openjdk-7-jre oracle-java8-installer icedtea-7-plugin openjdk-7-jre:i386 flashplugin-installer unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller libxine1-ffmpeg mencoder flac faac faad sox ffmpeg2theora libmpeg2-4 uudeview libmpeg3-1 mpeg3-utils mpegdemux liba52-dev mpeg2dec vorbis-tools id3v2 mpg321 mpg123 libflac++6 totem-mozilla icedax lame libmad0 libjpeg-progs libdvdcss2 libdvdread4 libdvdnav4 libswscale-extra-2 ubuntu-restricted-extras ubuntu-wallpapers* skype-wrapper git curl libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev build-essential &&
 
 #git config --global user.name "NewUser"
 #git config --global user.email newuser@example.com
