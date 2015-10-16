31584e7e3a5f14c050c50aa66cb0d9e746f960b5
Viktor Matushevskyi <viktorminator@gmail.com>
2015-10-16 14:10:36 +0200

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -20,8 +20,7 @@ sudo add-apt-repository -y ppa:otto-kesselgulasch/gimp &&
 sudo add-apt-repository -y ppa:gnome3-team/gnome3 &&
 sudo add-apt-repository -y ppa:webupd8team/java &&
 sudo add-apt-repository -y ppa:webupd8team/y-ppa-manager &&
-sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
-sudo add-apt-repository -y ppa:webupd8team/brackets &&
+sudo add-apt-repository -y ppa:webupd8team/sublime-text-3 &&
 sudo add-apt-repository -y ppa:maarten-baert/simplescreenrecorder &&
 sudo add-apt-repository -y ppa:webupd8team/atom &&
 sudo add-apt-repository -y ppa:git-core/ppa &&
@@ -37,7 +36,7 @@ sudo apt-get install libxss1 libappindicator1 libindicator7 &&
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&
 sudo dpkg -i google-chrome*.deb &&
 
-sudo apt-get install atom chromium-browser filezilla wine simplescreenrecorder brackets qt4-qtconfig golang mercurial qbittorrent synaptic vlc gimp gimp-data gimp-plugin-registry gimp-data-extras y-ppa-manager bleachbit openjdk-7-jre oracle-java8-installer icedtea-7-plugin openjdk-7-jre:i386 flashplugin-installer unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller libxine1-ffmpeg mencoder flac faac faad sox ffmpeg2theora libmpeg2-4 uudeview libmpeg3-1 mpeg3-utils mpegdemux liba52-dev mpeg2dec vorbis-tools id3v2 mpg321 mpg123 libflac++6 totem-mozilla icedax lame libmad0 libjpeg-progs libdvdcss2 libdvdread4 libdvdnav4 libswscale-extra-2 ubuntu-restricted-extras ubuntu-wallpapers* skype-wrapper git curl libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev build-essential &&
+sudo apt-get install atom chromium-browser filezilla wine simplescreenrecorder qt4-qtconfig golang mercurial qbittorrent synaptic vlc gimp gimp-data gimp-plugin-registry gimp-data-extras y-ppa-manager bleachbit openjdk-7-jre oracle-java8-installer icedtea-7-plugin openjdk-7-jre:i386 flashplugin-installer unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller libxine1-ffmpeg mencoder flac faac faad sox ffmpeg2theora libmpeg2-4 uudeview libmpeg3-1 mpeg3-utils mpegdemux liba52-dev mpeg2dec vorbis-tools id3v2 mpg321 mpg123 libflac++6 totem-mozilla icedax lame libmad0 libjpeg-progs libdvdcss2 libdvdread4 libdvdnav4 libswscale-extra-2 ubuntu-restricted-extras ubuntu-wallpapers* skype-wrapper git curl libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev build-essential &&
 
 sudo apt-get dist-upgrade &&
 #git config --global user.name "NewUser"
