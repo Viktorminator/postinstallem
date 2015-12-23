8159d5636ea6d045c83b668ab58bbbe44dfc5c42
Viktor Matushevskyi <viktorminator@gmail.com>
2015-12-23 12:58:58 +0000

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -15,28 +15,20 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stablec main" >> /etc/apt/sources.list.d/google-chrome.list' &&
 
 sudo add-apt-repository -y ppa:qbittorrent-team/qbittorrent-stable &&
-sudo add-apt-repository -y ppa:videolan/stable-daily &&
 sudo add-apt-repository -y ppa:otto-kesselgulasch/gimp &&
 sudo add-apt-repository -y ppa:gnome3-team/gnome3 &&
 sudo add-apt-repository -y ppa:webupd8team/java &&
 sudo add-apt-repository -y ppa:webupd8team/y-ppa-manager &&
 sudo add-apt-repository -y ppa:webupd8team/sublime-text-3 &&
 sudo add-apt-repository -y ppa:maarten-baert/simplescreenrecorder &&
-sudo add-apt-repository -y ppa:webupd8team/atom &&
 sudo add-apt-repository -y ppa:git-core/ppa &&
-sudo apt-add-repository -y ppa:chris-lea/node.js &&
 sudo add-apt-repository -y ppa:libreoffice/ppa &&
-sudo add-apt-repository -y ppa:andykimpe/skype
-
-echo 'deb http://download.videolan.org/pub/debian/stable/ /' | sudo tee -a /etc/apt/sources.list.d/libdvdcss.list &&
-echo 'deb-src http://download.videolan.org/pub/debian/stable/ /' | sudo tee -a /etc/apt/sources.list.d/libdvdcss.list &&
-wget -O - http://download.videolan.org/pub/debian/videolan-apt.asc|sudo apt-key add - &&
 
 sudo apt-get install libxss1 libappindicator1 libindicator7 &&
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&
 sudo dpkg -i google-chrome*.deb &&
 
-sudo apt-get install atom chromium-browser filezilla wine simplescreenrecorder qt4-qtconfig golang mercurial qbittorrent synaptic vlc gimp gimp-data gimp-plugin-registry gimp-data-extras y-ppa-manager bleachbit openjdk-7-jre oracle-java8-installer icedtea-7-plugin openjdk-7-jre:i386 flashplugin-installer unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller libxine1-ffmpeg mencoder flac faac faad sox ffmpeg2theora libmpeg2-4 uudeview libmpeg3-1 mpeg3-utils mpegdemux liba52-dev mpeg2dec vorbis-tools id3v2 mpg321 mpg123 libflac++6 totem-mozilla icedax lame libmad0 libjpeg-progs libdvdcss2 libdvdread4 libdvdnav4 libswscale-extra-2 ubuntu-restricted-extras ubuntu-wallpapers* skype-wrapper git curl libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev build-essential &&
+sudo apt-get install atom chromium-browser filezilla wine simplescreenrecorder qbittorrent synaptic vlc gimp gimp-data gimp-plugin-registry gimp-data-extras y-ppa-manager bleachbit openjdk-7-jre oracle-java8-installer icedtea-7-plugin openjdk-7-jre:i386 flashplugin-installer unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller libxine1-ffmpeg mencoder flac faac faad sox ffmpeg2theora libmpeg2-4 uudeview libmpeg3-1 mpeg3-utils mpegdemux liba52-dev mpeg2dec vorbis-tools id3v2 mpg321 mpg123 libflac++6 totem-mozilla icedax lame libmad0 libjpeg-progs libdvdcss2 libdvdread4 libdvdnav4 libswscale-extra-2 ubuntu-restricted-extras ubuntu-wallpapers* git curl libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev build-essential &&
 
 sudo apt-get dist-upgrade &&
 #git config --global user.name "NewUser"
